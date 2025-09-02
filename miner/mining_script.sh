336d96f86bb9121a9fd4445d3d624558ed1aa9ee
amkelly97 <alec.kelly115@gmail.com>
2025-09-02 13:00:05 -0400

    Revised for loop syntax

--- miner/mining_script.sh
+++ miner/mining_script.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
 #Source code provided in class September 2
-for i $(seq 63356); do
+for i in {10..63356}; do
 	saltword="$i"Wednesday;
-	hashval=$( echo -n $saltword | sha256sum )
-	echo "$hashval, $saltword" | grep '^0000'
+	hashval=$( echo -n $saltword | sha256sum );
+	echo "$hashval, $saltword" | grep '^0000';
 done
