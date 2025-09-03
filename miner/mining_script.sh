3a980c774aa9a7d43b99697a598b5a385bd8caac
amkelly97 <alec.kelly115@gmail.com>
2025-09-03 14:11:31 -0400

    Replace grep with an if statement, and expand to 400,000 iterations

--- miner/mining_script.sh
+++ miner/mining_script.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
 #Source code provided in class September 2
-for i in {10..63356}; do
-	saltword="$i"Wednesday;
-	hashval=$( echo -n $saltword | sha256sum );
-	echo "$hashval, $saltword" | grep '^0000';
+for i in (seq 400000); do
+	saltword="{$i}Wednesday"
+	hashval=$( echo -n $saltword | sha256sum )
+ 	if [[ $hashval == 0000* ]]; then
+ 		echo "$hashval, $saltword"
+   	fi
 done
